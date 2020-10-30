8de7c214580175fa33ca96381989ebb75701d8f5
richiaprilio23 <richiaprilioleather@gmail.com>
2020-10-30 14:33:12 +0700

    End of section 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl: './app.component.html', //panggil file ini
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit {
+  title = 'Dating App panggil dengan variable';
+  users: any; //jenis keamanan
+
+  constructor(private http: HttpClient) //http akses ke dotnet5 secara asynchronous
+  {}
+
+  //method main
+  ngOnInit() {
+   this.GetUsers();
+  }
+
+  //method GetUsers
+  GetUsers() {
+    this.http.get('https://localhost:5001/api/users')
+    .subscribe(
+      Response => {
+        this.users = Response;
+      },
+      error => {
+        console.log(error);
+      }
+      
+    ) //response callbacks
+  }
 }
